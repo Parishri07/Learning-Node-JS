40e53d6809b9afd0ef42eb11e690daae399eaaa7
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-10-29 18:20:52 +0530

    Update app.js

--- first-node-app/app.js
+++ first-node-app/app.js
@@ -1,13 +1,42 @@
-//File System
+const http = require('http');//require keyword is used to import some path to file or module
 const fs = require('fs');
-fs.writeFileSync('first.txt', "Hello from Node JS");
 
-const http = require('http');//to import some path to file or module
-const server = http.createServer((req,res)=>{
-    console.log(req);
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === "/") {
+        res.write('<html>');
+        res.write('<head>Enter message</head>');
+        res.write('<body><form action="/message" method="POST"><input type="text" name="msg"></input><button type="submit">Send</button></form></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if (url === "/message" && method === "POST") {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            // fs.writeFileSync('message.txt', message); //sync stands for synchronous, blocks execution of next line of code until that operation is done
+            fs.writeFile('message.txt', message, (err)=>{
+                res.statusCode = 302; //this implies that the page is redirected
+                res.setHeader('Location', '/');
+                return res.end();
+            });
+        })
+    }
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<html>');
+    res.write('<head>My first page</head>');
+    res.write('<body><h1>Learning Node</h1></body>');
+    res.write('</html>');
+    res.end();
 });
-server.listen(3000);
 
+server.listen(3000);
 
 
 
