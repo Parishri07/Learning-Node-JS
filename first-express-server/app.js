df5f36b0199cd77afe3cd7564586f399700260a5
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-02 13:48:33 +0530

    Update app.js

--- first-express-server/app.js
+++ first-express-server/app.js
@@ -4,6 +4,17 @@ const express = require('express');
 
 const app = express();
 
-const server = http.createServer();
+app.use((req, res, next) =>{
+    console.log("In middleware");
+    next(); //Allows the request to continue in the next middleware in line
+})
+
+app.use((req, res, next) =>{
+    console.log("In another middleware");
+    res.send('<h1>Hello.. Welcome to my express server</h1>');
+    //you travel form middleware to middleware using next() function. At the end of the last middleware you send a response
+})
+
+const server = http.createServer(app);
 
 server.listen(3000);
