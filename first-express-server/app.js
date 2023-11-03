afffc34d0edd65ea0bbee1405c7d913c7e72c1a8
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-03 16:29:09 +0530

    Update app.js

--- first-express-server/app.js
+++ first-express-server/app.js
@@ -1,20 +1,17 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) =>{
+app.use('/add-product', (req, res, next) =>{
     console.log("In middleware");
-    next(); //Allows the request to continue in the next middleware in line
+    res.send('<h1>This is Add Products Page</h1>');
+    // next(); //Allows the request to continue in the next middleware in line
 })
 
-app.use((req, res, next) =>{
+app.use('/', (req, res, next) =>{
     console.log("In another middleware");
     res.send('<h1>Hello.. Welcome to my express server</h1>');
-    //you travel from middleware to middleware using next() function. At the end of the last middleware you send a response
+    //you travel form middleware to middleware using next() function. At the end of the last middleware you send a response
 })
 
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
