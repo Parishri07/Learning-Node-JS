f4bb20cc63f501c55c3b9ec434745764c299dc94
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-08 23:48:58 +0530

    Update shop.js

--- first-express-server/routes/shop.js
+++ first-express-server/routes/shop.js
@@ -7,12 +7,13 @@ const rootDir = require('../utils/path');
 
 const routes = express.Router();
 
-routes.get('/', (req, res, next) =>{
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  // '/views/shop.html' is not a valid path because the '/' at the beginning searches the folder in our operating system so we need to add path to our project before searching for views folder 
+routes.get('/', (req, res, next) => {
+  const products = adminData.products;
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop', {prods: products, docTitle: 'Shop'});
 })
- //you travel from middleware to middleware using next() function. At the end of the last middleware you send a response, because you cannot send two res.send functions
- //get function checks for the exact path also
+// '/views/shop.html' is not a valid path because the '/' at the beginning searches the folder in our operating system so we need to add path to our project before searching for views folder 
+//you travel from middleware to middleware using next() function. At the end of the last middleware you send a response, because you cannot send two res.send functions
+//get function checks for the exact path also
 
 module.exports = routes;
