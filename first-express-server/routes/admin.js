4014f97d0d9f06f181e265be7b3292ef1944bb7d
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-09 22:51:07 +0530

    Update admin.js

--- first-express-server/routes/admin.js
+++ first-express-server/routes/admin.js
@@ -4,19 +4,27 @@ const express = require('express');
 
 const rootDir = require('../utils/path');
 
-const routes = express.Router();
+const router = express.Router();
 
 const products = [];
 
 // /admin/add-product => GET
-routes.get('/add-product', (req, res, next) =>{
-     res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
+router.get('/add-product', (req, res, next) => {
+    res.render('add-product', 
+    { 
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        formsCSS: true,
+        productCSS: true,
+        activeAddProduct: true
+    })
+    // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     // next(); //Allows the request to continue in the next middleware in line
 })
 
 // /admin/add-product => POST
-routes.post('/add-product', (req, res, next) =>{
-    products.add({title: req.body.title});
+router.post('/add-product', (req, res, next) => {
+    products.push({ title: req.body.title });
     res.redirect('/');
 })
 
