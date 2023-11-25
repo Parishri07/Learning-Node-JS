acaec63c0ad3159c59c4854375353d4ac3cfce9f
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-25 16:19:35 +0530

    Update admin.js

--- first-express-server/routes/admin.js
+++ first-express-server/routes/admin.js
@@ -4,15 +4,17 @@ const express = require('express');
 
 // const rootDir = require('../utils/path');
 
-const productControllers = require('../controllers/products');
+const adminControllers = require('../controllers/admin');
 
 const router = express.Router();
 
 // /admin/add-product => GET
-router.get('/add-product', productControllers.getAddProduct);
+router.get('/add-product', adminControllers.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminControllers.getProducts);
 
 // /admin/add-product => POST
-router.post('/add-product', productControllers.postAddProduct);
+router.post('/add-product', adminControllers.postAddProduct);
 
 module.exports = router;
