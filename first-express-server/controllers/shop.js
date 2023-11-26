5ac7b6b87cf794147ca30a11775e5a610cccb0d0
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-26 14:33:33 +0530

    Update shop.js

--- first-express-server/controllers/shop.js
+++ first-express-server/controllers/shop.js
@@ -35,6 +35,13 @@ exports.getCart = (req, res, next) => {
   })
 }
 
+exports.getOrders = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  })
+}
+
 exports.getCheckout = (req, res, next) => {
   res.render('shop/checkout', {
     path: '/checkout',
