8dd1f76d5dcc869c731c057793121861463bf7db
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-26 14:34:34 +0530

    Update product.js

--- first-express-server/models/product.js
+++ first-express-server/models/product.js
@@ -5,32 +5,34 @@ const rootDir = require('../utils/path');
 
 const p = path.join(rootDir, 'data', 'products.json');
 
+const getProductsFromFile = cb => {
+    fs.readFile(p, (err, fileContent) => {
+        if(err){
+            cb([]);
+        }
+        cb(JSON.parse(fileContent));
+    });
+}
+
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
+    constructor(title, imageUrl, description, price) {
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
     }
 
     save() {
-        fs.readFile(p, (err, fileContent) =>  {
-            let products = [];
-            if(!err){
-                products = JSON.parse(fileContent);
-            }
+        getProductsFromFile( products => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        });
+        })
     }
 
     static fetchAll(cb) {
-        fs.readFile(p, (err, fileContent) => {
-            if(err){
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
+       getProductsFromFile(cb);
     }
     // static function can be called directly from the class without creating an instance of the class, to display all the elements of the products array we dont need to make a dummy object then call this function
-
 };
