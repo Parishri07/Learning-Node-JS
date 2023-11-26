d633cd01550b647b6a44d7a5d7d8d78dbfd8aa4c
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2023-11-26 14:32:49 +0530

    Update admin.js

--- first-express-server/controllers/admin.js
+++ first-express-server/controllers/admin.js
@@ -4,10 +4,10 @@ exports.getAddProduct = (req, res, next) => {
     res.render('admin/add-product', 
     { 
         pageTitle: 'Add Product', 
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
+        path: '/admin/add-product'
+        // formsCSS: true,
+        // productCSS: true,
+        // activeAddProduct: true
     })
     // res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
     // next(); //Allows the request to continue in the next middleware in line
@@ -15,7 +15,11 @@ exports.getAddProduct = (req, res, next) => {
 // res.render function is used to insert a template. First it looks for the view engine and then inserts the file name given as the first parameter as the template
 
 exports.postAddProduct = (req, res, next) => {
-  const product = new Product(req.body.title);
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  const product = new Product(title, imageUrl, price, description);
   product.save();
   res.redirect('/');
 }
