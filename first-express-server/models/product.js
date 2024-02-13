59e662cd9a700b42955288da5a04ecbf18b61908
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-13 20:21:04 +0530

    connecting to mysql database to fetch and insert data

--- first-express-server/models/product.js
+++ first-express-server/models/product.js
@@ -75,3 +75,34 @@ module.exports = class Product {
         });
     };
 };
+
+/* reaching out to the mysql database rather than local file
+
+const db = require('../utils/database');
+
+const Cart = require('./cart');
+
+module.exports = class Product {
+    constructor(id, title, imageUrl, description, price) {
+        this.id = id;
+        this.title = title;
+        this.imageUrl = imageUrl;
+        this.description = description;
+        this.price = price;
+    }
+
+    save(){
+        return db.execute('INSERT INTO products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)', [this.title, this.price, this.imageUrl, this.description])
+    }
+
+    static deletebyId(id){
+    };
+
+    static fetchAll(){
+        return db.execute('SELECT * FROM products')
+    };
+
+    static findById(id){
+        return db.execute('SELECT * FROM products WHERE products.id = ?', [id]);    
+    };
+}; */
