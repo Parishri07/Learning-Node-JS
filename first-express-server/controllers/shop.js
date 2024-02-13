bfda4526c4f879adf35dd04a8371c801daf12657
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-13 20:18:34 +0530

    using mysql database instead of local file

--- first-express-server/controllers/shop.js
+++ first-express-server/controllers/shop.js
@@ -90,3 +90,42 @@ exports.getCheckout = (req, res, next) => {
   })
 };
 
+/* using mysql database
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll()
+   .then(([rows, fieldData]) => {
+       res.render('shop/product-list', {
+        prods: rows,
+        pageTitle: 'All Products',
+        path: '/products'
+    });
+   })
+   .catch(err => { console.log(err) });
+ };
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll()
+   .then(([rows, fieldData]) => {
+       res.render('shop/index', {
+        prods: rows,
+        pageTitle: 'Shop',
+        path: '/'
+    });
+   })
+   .catch(err => { console.log(err) });
+ }; 
+ 
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId;
+  Product.findById(prodId)
+    .then(([product]) => {
+      res.render('shop/product-detail', {
+        product: product[0],
+        pageTitle: product[0].title,
+        path: '/products'
+      });
+    })
+    .catch(err => console.log(err));
+  }; 
+*/
