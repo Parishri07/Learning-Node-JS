44ae20c0e75ff9d48da441345247a54a4cba37b9
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-16 00:52:43 +0530

    Update admin.js

--- first-express-server/controllers/admin.js
+++ first-express-server/controllers/admin.js
@@ -90,3 +90,90 @@ exports.postAddProduct = (req, res, next) => {
     .catch(err => console.log(err));
 } 
 */
+
+/* using sequelize
+   
+exports.postAddProduct = (req, res, next) => {
+  const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
+  Product.create({
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch(err => console.log(err))
+}
+
+exports.getProducts = (req, res, next) => {
+  Product.findAll()
+    .then(products => {
+      res.render('admin/products', {
+        prods: products,
+        pageTitle: 'Admin Products',
+        path: '/admin/products'
+      });
+    })
+    .catch(err => console.log(err));
+}
+
+exports.getEditProduct = (req, res, next) => {
+  const editMode = req.query.edit;
+  //we can access the query parameters by using req.query, query parameters are passed in url after ? and are key value pairs. Query paramters are generally used for keeping some of the filters user enters
+  if (!editMode) {
+    return res.redirect('/');
+  }
+  const prodId = req.params.productId;
+  Product.findById(prodId) 
+    .then(product => {
+      if (!product) {
+        res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      })
+    })
+    .catch(err => console.log(err));
+}
+
+exports.postEditProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  Product.findById(prodId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDesc;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(() => {
+      res.redirect('/admin/products');  
+    })
+    .catch(err => console.log(err) );
+}
+
+exports.postDeleteProduct = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findbyId(prodId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(() => {
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err) );
+  });
+}
+*/
