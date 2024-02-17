70438af45bb2e068dcefbd6a1390bb7b1ceebe9c
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-17 23:55:09 +0530

    Create cart-item.js

---
+++ first-express-server/models/cart-item.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database.js');
+
+const Cart = sequelize.define('cartItem', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    quantity: Sequelize.INTEGER
+});
+
+module.exports = CartItem;
