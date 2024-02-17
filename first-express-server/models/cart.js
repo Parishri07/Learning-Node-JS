a25c142bc8e71ac111a1889e17f768311e202e9e
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-17 23:54:28 +0530

    Update cart.js

--- first-express-server/models/cart.js
+++ first-express-server/models/cart.js
@@ -67,3 +67,20 @@ module.exports = class Cart {
         })
     }
 }
+
+/*
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database.js');
+
+const Cart = sequelize.define('cart', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Cart;
+*/
