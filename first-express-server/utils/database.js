03ab9586cbb05988817591c1397f3a3b6a107e6c
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-23 13:15:51 +0530

    Update database.js

--- first-express-server/utils/database.js
+++ first-express-server/utils/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/* const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     host: 'localhost',
@@ -7,4 +7,12 @@ const pool = mysql.createPool({
     password: ''
 })
 
-module.exports = pool.promise();
+module.exports = pool.promise(); */
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('(database name)', '(root username)', '(password)', {
+    dialect: 'mysql', 
+    host: 'localhost'
+});
+
+module.exports = sequelize;
