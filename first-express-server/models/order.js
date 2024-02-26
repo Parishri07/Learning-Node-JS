c75d70fdc5c0fd4b69007b6f8d3a2cfa21d88a98
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-26 23:11:56 +0530

    Create order.js

---
+++ first-express-server/models/order.js
@@ -0,0 +1,14 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../utils/database.js');
+
+const Order = sequelize.define('order', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    }
+});
+
+module.exports = Order;
